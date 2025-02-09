5ede40181a4e3a690f08647b672ed7a313c938e4
Kirti Tiwari <tiwarsha@gmail.com>
2025-02-09 21:35:06 +0530

    workflow2

--- src/main/java/com/example/tests/BaseTest.java
+++ src/main/java/com/example/tests/BaseTest.java
@@ -10,6 +10,11 @@ public class BaseTest {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
 // this is my code that i need to practice and sit behined the scenes.
+// this is my code that i need to practice and sit behined the scenes.
+// this is my code that i need to practice and sit behined the scenes.
+// this is my code that i need to practice and sit behined the scenes.
+// this is my code that i need to practice and sit behined the scenes.
+
         driver.manage().window().maximize();
     }
 
